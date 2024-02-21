123149fd633bd9c38348951e812eb40b0c8ffa22
Dr. Christian Geuer-Pollmann <chgeuer@users.noreply.github.com>
2024-02-21 10:15:34 +0100

    Yikes, v0.1.0 is available on hex, yeah

---
+++ checksum-Elixir.Dot.Native.exs
@@ -0,0 +1,6 @@
+%{
+  "ex_dot-v0.1.0-nif-2.15-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:f9534dc2b88b83877362e56b2b2fb5e0da2ddeb636fac0375862108d92d231fb",
+  "ex_dot-v0.1.0-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:e1f330da4e2ea4a4feb94b1614006b886af24253c7986a9ae37b57494cef87c5",
+  "libex_dot-v0.1.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:e649545980f54e26d9e9ae8a3f6611210c271251a998c909c6c824202dbf412a",
+  "libex_dot-v0.1.0-nif-2.15-x86_64-unknown-linux-gnu.dll.tar.gz" => "sha256:e649545980f54e26d9e9ae8a3f6611210c271251a998c909c6c824202dbf412a",
+}
--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule ExDotRust.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/chgeuer/ex_dot"
-  @version "0.1.0"
+  @version "0.1.1-dev"
   @dev? String.ends_with?(@version, "-dev")
   @force_build? System.get_env("EX_DOT_BUILD") in ["1", "true"]
 
